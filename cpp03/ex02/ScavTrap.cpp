39b6339e586ae468deab706b3bf03c31e7b22b51
imane haffout <haffout.iman@gmail.com>
2024-12-19 00:34:39 +0100

    add cpp03

---
+++ cpp03/ex02/ScavTrap.cpp
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ihaffout <ihaffout@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/14 12:26:53 by ihaffout          #+#    #+#             */
+/*   Updated: 2024/12/18 20:57:23 by ihaffout         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScavTrap.hpp"
+
+ScavTrap::ScavTrap()
+{
+    std::cout << "Default ScavTrap constructor called!" << std::endl;
+    this->name = "Default";
+    this->hitPoints = 100;
+    this->energyPoints = 50;
+    this->attackDamage = 20;
+}
+
+ScavTrap::ScavTrap(const std::string &name)
+{
+    std::cout << "Parameterized ScavTrap constructor called!" << std::endl;
+    this->name = name;
+    this->hitPoints = 100;
+    this->energyPoints = 50;
+    this->attackDamage = 20;
+}
+
+ScavTrap::ScavTrap(const ScavTrap &obj)
+{
+    *this = obj;
+}
+
+ScavTrap&   ScavTrap::operator=(const ScavTrap &obj)
+{
+    if(this != &obj)
+        ClapTrap::operator=(obj);
+    return(*this);
+}
+
+ScavTrap::~ScavTrap()
+{
+    std::cout << "ScavTrap Destructor called!" << std::endl;
+}
+
+
+void ScavTrap::attack(const std::string &target)
+{
+    if(!this->energyPoints)
+        std::cout << "ScavTrap " << this->name << " can't attack " << target << ", energy points is not enough!" << std::endl;
+    else if (!this->hitPoints)
+        std::cout << "ScavTrap " << this->name << " can't attack " << target << ", hit points is not enough!" << std::endl;
+    else
+    {
+        std::cout << "ScavTrap " << this->name << " attacks " << target << ", causing " << this->attackDamage << " points of damage!" << std::endl;
+        this->energyPoints--;
+    }
+    return;
+}
+
+void ScavTrap::guardGate()
+{
+    std::cout << "ScavTrap " << this->name << " is now in Gate keeper mode!" << std::endl;
+}
