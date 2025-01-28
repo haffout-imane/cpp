950bcf55e30ca2ab58ea45c5365c7d128e883116
imane haffout <haffout.iman@gmail.com>
2025-01-28 15:08:15 +0100

    Update Intern.hpp

--- cpp05/ex03/Intern.hpp
+++ cpp05/ex03/Intern.hpp
@@ -10,3 +10,31 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef INTERN_HPP
+# define INTERN_HPP
+
+    #include "AForm.hpp"
+    #include "ShrubberyCreationForm.hpp"
+    #include "RobotomyRequestForm.hpp"
+    #include "PresidentialPardonForm.hpp"
+
+class Intern
+{
+    public:
+        Intern();
+        Intern(Intern const &obj);
+        ~Intern();
+        Intern & operator=(Intern const &obj);
+        AForm *makeForm(std::string form, std::string target);
+        class FormNotFoundException : public std::exception
+        {
+            public:
+                virtual const char *what() const throw();
+        };
+};  
+
+AForm *newShrubberyCreationForm(std::string target);
+AForm *newRobotomyRequestForm(std::string target);
+AForm *newPresidentialPardonForm(std::string target);
+
+#endif
