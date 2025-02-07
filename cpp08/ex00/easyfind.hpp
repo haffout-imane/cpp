74e0c9224492eb5eedc4a7bdade240208370ddb3
imane haffout <haffout.iman@gmail.com>
2025-02-07 23:59:57 +0100

    Add files via upload

---
+++ cpp08/ex00/easyfind.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ihaffout <ihaffout@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/07 18:21:33 by ihaffout          #+#    #+#             */
+/*   Updated: 2025/02/07 21:32:24 by ihaffout         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+    #include "iostream"
+
+template<typename T>
+void easyfind(T p1, int p2);
+
+    #include "easyfind.tpp"
+
+#endif
