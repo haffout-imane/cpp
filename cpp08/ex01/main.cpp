fd0cba1a7bf03cd1a6ff6b7e904735651cb64a80
imane haffout <haffout.iman@gmail.com>
2025-02-08 03:29:03 +0100

    Add files via upload

---
+++ cpp08/ex01/main.cpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ihaffout <ihaffout@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/08 01:39:20 by ihaffout          #+#    #+#             */
+/*   Updated: 2025/02/08 03:26:16 by ihaffout         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Span.hpp"
+
+int main()
+{
+    try
+    {
+        Span sp = Span(10);
+        sp.addNumber(3);
+        sp.addNumber(5);
+        sp.addNumber(12);
+        sp.addNumber(13);
+        std::cout << "Shortest span: " << sp.shortestSpan() << std::endl;
+        std::cout << "Longest span: " << sp.longestSpan() << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    std::cout << "--------------------------------" << std::endl;
+    try
+    {
+        std::vector<int> v;
+        for(int i = 0; i < 10000; i++)
+            v.push_back(i);
+        Span sp = Span(10000);
+        sp.addRange(v.begin(), v.end());
+        std::cout << "Shortest span: " << sp.shortestSpan() << std::endl;
+        std::cout << "Longest span: " << sp.longestSpan() << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    return 0;
+}
