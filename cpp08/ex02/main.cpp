6a61f6e7bf513e1b1bd4f870c57a027fc6d9f9e6
imane haffout <haffout.iman@gmail.com>
2025-02-11 06:07:48 +0100

    Update main.cpp

--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ihaffout <ihaffout@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/08 03:38:26 by ihaffout          #+#    #+#             */
-/*   Updated: 2025/02/08 05:36:36 by ihaffout         ###   ########.fr       */
+/*   Updated: 2025/02/10 05:44:03 by ihaffout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,30 +23,17 @@ int main()
     mstack.push(3);
     mstack.push(5);
     mstack.push(737);
-    //[...]
     mstack.push(0);
+    
     MutantStack<int>::iterator it = mstack.begin();
     MutantStack<int>::iterator ite = mstack.end();
     ++it;
     --it;
     while (it != ite)
     {
-    std::cout << *it << std::endl;
-    ++it;
+        std::cout << *it << std::endl;
+        ++it;
     }
     std::stack<int> s(mstack);
-
-    std::cout << "---------------------" << std::endl;
-
-    std::list<int> list;
-    list.push_back(5);
-    list.push_back(17);
-    std::cout << list.back() << std::endl;
-    list.pop_back();
-    std::cout << list.size() << std::endl;
-    list.push_back(3);
-    list.push_back(5);
-    list.push_back(737);
-    
     return 0;
 }
