5d5c6fbff576c0d21470e4e3154f237947865383
imane haffout <haffout.iman@gmail.com>
2025-02-13 02:42:07 +0100

    Update Span.hpp

--- cpp08/ex01/Span.hpp
+++ cpp08/ex01/Span.hpp
@@ -6,14 +6,17 @@
 /*   By: ihaffout <ihaffout@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/08 01:16:44 by ihaffout          #+#    #+#             */
-/*   Updated: 2025/02/08 02:11:53 by ihaffout         ###   ########.fr       */
+/*   Updated: 2025/02/12 05:02:11 by ihaffout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SPAN_HPP
 #define SPAN_HPP
+
     #include <iostream>
     #include <vector>
+    #include <algorithm>
+    
 class Span
 {
     private:
@@ -21,7 +24,7 @@ class Span
         std::vector<int> span;
     public:
         Span();
-        Span(int N);
+        Span(unsigned int N);
         Span(Span const & src);
         ~Span();
         Span & operator=(Span const & src);
@@ -30,8 +33,5 @@ class Span
         int longestSpan();
         void addRange(std::vector<int>::iterator begin, std::vector<int>::iterator end);  
 };
-
-
-
 
 #endif
