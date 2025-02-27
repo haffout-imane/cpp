d159cd7da4cac1f3bc652578ec89e346e1f26663
imane haffout <haffout.iman@gmail.com>
2025-02-27 02:39:28 +0000

    Update BitcoinExchange.hpp

--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: ihaffout <ihaffout@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 02:29:15 by ihaffout          #+#    #+#             */
-/*   Updated: 2025/02/14 02:29:42 by ihaffout         ###   ########.fr       */
+/*   Updated: 2025/02/22 20:59:57 by ihaffout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,21 @@
 #define BITCOINEXCHANGE_HPP
 
     #include <iostream>
+    #include <fstream>
+    #include <map>
+    
+class BitcoinExchange
+{
+    private:
+        std::map<std::string, float> _data;
 
+    public:
+        BitcoinExchange();
+        BitcoinExchange(BitcoinExchange const & src);
+        BitcoinExchange & operator=(BitcoinExchange const & src);
+        ~BitcoinExchange();
+        void exchange(std::string dataFile, char *inputFile);
 
+};
 
 #endif
