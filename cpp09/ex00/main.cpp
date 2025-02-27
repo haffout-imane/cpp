83b0dfb58a375d2efe977b8dc703fdc8192d145a
imane haffout <haffout.iman@gmail.com>
2025-02-27 02:39:57 +0000

    Update main.cpp

--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -6,13 +6,26 @@
 /*   By: ihaffout <ihaffout@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 02:26:33 by ihaffout          #+#    #+#             */
-/*   Updated: 2025/02/14 02:27:24 by ihaffout         ###   ########.fr       */
+/*   Updated: 2025/02/27 02:15:27 by ihaffout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
-int main()
+int main(int ac, char **av)
 {
-    
+    if (ac != 2)
+    {
+        std::cout << "Error: could not open file." << std::endl;
+        return 1;
+    }
+    BitcoinExchange exchange;
+    try
+    {
+        exchange.exchange("data.csv", av[1]);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 }
